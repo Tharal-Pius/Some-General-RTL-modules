60f547fdf34116a5acd9e63272812aa3955ed85c
Tharal-Pius <tharalpius@gmail.com>
2024-10-19 19:24:44 +0530

    Update Asynch_FIFO.v
    updated to use even the last slot in FIFO

--- Asynch FIFO/RTL+TB/Asynch_FIFO.v
+++ Asynch FIFO/RTL+TB/Asynch_FIFO.v
@@ -1,55 +1,47 @@
-// Code your design here
 module Asynch_FIFO #(
-    parameter data_width = 8,
-    parameter FIFO_depth = 16
+    parameter depth = 16,
+    parameter width = 32,
+    parameter addr_size = $clog2(depth) + 1  // MSB extra bit for full/empty distinction
 ) (
-    input  [(data_width - 1):0] data_in,
-    input wr_CLK, rd_CLK, RST, wr_en, rd_en,
-    output  reg [(data_width - 1):0] data_out,
-    output full, empty
+    input [(width - 1):0] data_in,
+    input rst_n,
+    input sys_clk,
+    output logic [(width-1):0] data_out,
+    output logic empty, 
+    output logic full
 );
-  localparam addr_width = $clog2(FIFO_depth);
-  
-  reg [(data_width-1):0] mem [0:(FIFO_depth-1)];
-  reg [(addr_width):0] FIFO_count = 4'b0;
-  reg [(addr_width-1):0] rd_ptr = 4'b0;
-  reg [(addr_width-1):0] wr_ptr = 4'b0;
-  integer i;
 
-  assign full = ((wr_ptr + 1'b1) == rd_ptr);
-  assign empty = (wr_ptr == rd_ptr);
+// FIFO memory array
+logic [0:(depth-1)] [(width - 1):0] FIFO_mem_rd_wr;
+logic [(addr_size-1):0] wr_ptr;  
+logic [(addr_size-1):0] rd_ptr;  
 
+always_comb begin
+    // Empty when pointers are equal (including the extra bit)
+    empty = (wr_ptr == rd_ptr);
 
-//write_data_in
-  always @(posedge wr_CLK or posedge RST) begin
-    if(RST) begin
-        for (i =0; i<FIFO_depth; i=i+1 ) begin
-            mem[i] <= 8'b0;     
+    // Full when the next write pointer catches up to the read pointer
+    full = ((wr_ptr[addr_size-2:0] == rd_ptr[addr_size-2:0]) && (wr_ptr[addr_size-1] != rd_ptr[addr_size-1]));
+end
+
+always_ff @(posedge sys_clk or negedge rst_n) begin
+    if (~rst_n) begin
+        wr_ptr <= {addr_size{1'b0}};
+        rd_ptr <= {addr_size{1'b0}};
+        data_out <= {width{1'b0}};
+    end else begin
+        // Read operation
+        if (!empty) begin
+            data_out <= FIFO_mem_rd_wr[rd_ptr[addr_size-2:0]];
+            rd_ptr <= rd_ptr + 1'b1;  
         end
-        wr_ptr <= 0;
-    end
-    else if(wr_en && ~full) begin
-      mem[wr_ptr] <= data_in;
-      wr_ptr <= wr_ptr +1;
-    end
-    else begin
-        wr_ptr <= wr_ptr;
-    end
-  end  
 
+        // Write operation
+        if (!full) begin
+            FIFO_mem_rd_wr[wr_ptr[addr_size-2:0]] <= data_in;
+            wr_ptr <= wr_ptr + 1'b1;  
+        end
+    end
+end
 
-//read_data_out
-  always @(posedge rd_CLK or posedge RST) begin
-    if(RST) begin
-            data_out <= 8'b0;
-            rd_ptr <= 0;
-    end
-    else if(rd_en & ~empty) begin
-      data_out <= mem[rd_ptr];
-      rd_ptr <= rd_ptr +1;
-    end
-    else begin
-        rd_ptr <= rd_ptr;
-    end
-  end
 endmodule
