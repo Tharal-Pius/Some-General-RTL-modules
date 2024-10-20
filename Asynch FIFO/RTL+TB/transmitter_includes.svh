4181c083de3ab44ca467bb7d08522566ed5206c5
Tharal-Pius <tharalpius@gmail.com>
2024-10-20 08:24:28 +0530

    update_new

--- Asynch FIFO/RTL+TB/transmitter_includes.svh
+++ Asynch FIFO/RTL+TB/transmitter_includes.svh
@@ -1,4 +1,4 @@
-`include "transmitter.sv"
-`include "double_flop_synch.sv"
-`include "bin_to_gray.sv"
-`include "gray_to_bin.sv"
+`include "transmitter.svh"
+`include "double_flop_synch.svh"
+`include "bin_to_gray.svh"
+`include "gray_to_bin.svh"
